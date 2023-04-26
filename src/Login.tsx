1bcf00b150f14d24594390d62dbe360772f18a88
ts7752 <ts7752@naver.com>
2023-04-26 17:04:16 +0900

    s

--- src/Login.tsx
+++ src/Login.tsx
@@ -14,6 +14,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import CryptoJS from "crypto-js";
 import { Dimensions } from "react-native";
+//teststestt
 
 const Stack = createStackNavigator();
 const windowWidth = Dimensions.get("window").width;
